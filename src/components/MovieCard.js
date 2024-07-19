73412820b091717fedfac62485b9fc9f070fd93f
Chaithanya Hasini Doopati <hasinichaithanya04@gmail.com>
2024-07-19 16:19:54 +0530

    second commit

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -2,9 +2,9 @@ import React, { useState, useEffect } from "react";
 import { v4 as uuid } from "uuid";
 import Card from "react-bootstrap/Card";
 import Image from "react-bootstrap/Image";
-import Spinner from "react-bootstrap/Spinner";
 
 import "./moviecard.css";
+
 const constants = {
   BEGIN: "begin",
   IN_PROGRESS: "inProgress",
@@ -21,15 +21,17 @@ const MovieCardComponent = (props) => {
     const fetchData = async () => {
       if (movie) {
         setStatus(constants.IN_PROGRESS);
-        const url = `https://openlibrary.org/search.json?q=${movie}`;
+        const url = `https://openlibrary.org/search.json?q=${encodeURIComponent(
+          movie
+        )}`;
         const result = await fetch(url);
         const res = await result.json();
-        console.log(res.docs[0]);
         if (res.docs && res.docs.length > 0) {
           setMoviesList(res.docs.slice(0, 10));
           await fetchDogImages();
           setStatus(constants.COMPLETE);
         } else {
+          setMoviesList([]);
           setStatus(constants.BEGIN);
         }
       }
@@ -51,20 +53,19 @@ const MovieCardComponent = (props) => {
   const renderMovies = () => {
     return moviesList.map((movie, index) => (
       <Card key={uuid()} className="movie-card">
-        {" "}
         <Image
           src={dogImageList[index]}
           alt="Random dog"
           className="dog-image"
           variant="top"
-        />{" "}
-        <Card.Body>
+        />
+        <Card.Body className="card-body" mt="2">
           <Card.Title>Title: {movie.title}</Card.Title>
           <Card.Text>
             Author:{" "}
             {movie.author_name ? movie.author_name.join(", ") : "Unknown"}
           </Card.Text>
-          <Card.Text>Number of Ratings:{movie.ratings_count}</Card.Text>
+          <Card.Text>Number of Ratings: {movie.ratings_count}</Card.Text>
         </Card.Body>
       </Card>
     ));
@@ -73,13 +74,13 @@ const MovieCardComponent = (props) => {
   return (
     <div className="main-container">
       {status === constants.BEGIN && <h1>Search here for the movies...</h1>}
-      {status === constants.IN_PROGRESS && (
-        <Spinner animation="grow" variant="dark" />
-      )}
+      {status === constants.IN_PROGRESS && <p>Loading...</p>}
       {status === constants.COMPLETE && (
         <div>
           <h3>Results for: {movie}</h3>
-          <div className="movies-container"> {renderMovies()}</div>
+          <div className="movies-container">
+            {moviesList.length > 0 ? renderMovies() : "No results found"}
+          </div>
         </div>
       )}
     </div>
